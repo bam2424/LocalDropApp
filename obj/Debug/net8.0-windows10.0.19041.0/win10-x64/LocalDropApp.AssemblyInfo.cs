107dece8b7acedf8a33d627fbb5f40f5f22f57a1
brandon <141857607+bam2424@users.noreply.github.com>
2025-06-04 14:26:00 -0400

    Replace bat file with BuildLocalDropApp.exe

--- obj/Debug/net8.0-windows10.0.19041.0/win10-x64/LocalDropApp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows10.0.19041.0/win10-x64/LocalDropApp.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LocalDropApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+5e0dd900cc9aede287d7a1c50cd993bc2202dcfe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+9a6acbcf1fe8174a7a45ffa4050b653a6ff0fac2")]
 [assembly: System.Reflection.AssemblyProductAttribute("LocalDropApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LocalDropApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
